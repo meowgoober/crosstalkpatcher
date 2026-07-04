7151a0058ebbaa9082611755c98fd9a598340133
Dorker <142069939+meowgoober@users.noreply.github.com>
2026-07-04 01:53:05 -0600

    update to the expiremental commit

--- RerouteInjector.cs
+++ RerouteInjector.cs
@@ -1,5 +1,6 @@
 namespace CrossTalkPatcher;
 
+/// AsmResolver was a pain to get working with, i tried to even to get to test with Yahoo Messenger but it wouldnt, when it did this program told me that it will only do .NET programs, i have NEVER seen a .net program that uses PE in its own other than retro messaging client, i hate this stupid plugin so much and this was a pain, this took me 2 whole days, and it was acting bad. This has been moved to Liefimportinjector.cs. - corncat 
 public static class RerouteInjector
 {
     public static void AddRerouteImport(string exePath, string dllName = "reroute.dll", string functionName = "ImportMe")
